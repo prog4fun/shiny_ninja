73f77b0614336e1223f3eb05bb3003031e0c32d9
Michael Koriath <development@koriath-tech.de>
2014-06-01 23:34:40 +0200

    Role Change
    fixes bug not able to change roles, parameter is permitted now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,13 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!
   # before_filter :add_breadcrumb_index
   load_and_authorize_resource
-  
+
   ### Index ###################################################################
-  
+
   def index
     params[:search] ||= {}
     @users = User.search(params[:search]).page(params[:page])
-    
+
     @active_menu = "user"
     @search_bar = true
 
@@ -20,14 +20,14 @@ class UsersController < ApplicationController
       format.html
     end
   end
-  
+
   ### Show ####################################################################
 
   def show
     @user = User.find(params[:id])
-    
+
     own_user = true if current_user.id == @user.id
-    if ( current_user.is? :administrator ) || own_user
+    if (current_user.is? :administrator) || own_user
       @active_menu = "user"
       # add_breadcrumb t("labels.actions.show"), user_path(@user)
 
@@ -39,12 +39,12 @@ class UsersController < ApplicationController
     end
 
   end
-  
+
   ### New #####################################################################
 
   def new
     @user = User.new
-    
+
     @active_menu = "user"
     # add_breadcrumb t("labels.actions.new"), new_user_path
 
@@ -52,46 +52,46 @@ class UsersController < ApplicationController
       format.html
     end
   end
-  
+
   ### Edit ####################################################################
 
   def edit
     @user = User.find(params[:id])
-    
+
     own_user = true if current_user.id == @user.id
-    if ( current_user.is? :administrator ) || own_user
+    if (current_user.is? :administrator) || own_user
       @active_menu = "user"
       # add_breadcrumb t("labels.actions.edit"), edit_user_path(@user.id)
     else
       not_own_object_redirection
     end
   end
-  
+
   ### Create ##################################################################
 
   def create
     @user = User.new(user_params)
-    
+
     @active_menu = "user"
-        
+
     respond_to do |format|
-      @user.attributes = {:country => "de" }
+      @user.attributes = {:country => "de"}
       if @user.save
         format.html { redirect_to @user, notice: t("confirmations.messages.saved") }
       else
         format.html { render action: "new" }
       end
     end
-              
+
   end
-  
+
   ### Update ##################################################################
 
   def update
     @user = User.find(params[:id])
-    
+
     @active_menu = "user"
-    
+
     respond_to do |format|
       params[:user][:country] = "de"
       if @user.update_attributes(user_params)
@@ -100,16 +100,16 @@ class UsersController < ApplicationController
         format.html { render action: "edit" }
       end
     end
-      
+
   end
-  
+
   ### Destroy #################################################################
 
   def destroy
     @user = User.find(params[:id])
-    
+
     @active_menu = "user"
-    
+
     dependency = @user.customers.count
     dependency += @user.services.count
     if dependency > 0
@@ -121,18 +121,18 @@ class UsersController < ApplicationController
         format.html { redirect_to users_url }
       end
     end
-    
+
   end
-  
+
   ### Other ###################################################################
-  
+
   private
   def add_breadcrumb_index
     add_breadcrumb t("labels.breadcrumbs.index"), users_path, :title => t("labels.breadcrumbs.index_title")
   end
-  
+
   def user_params
-	params.require(:user).permit(:city, :comment, :confirmed_at, :country, :email, :firstname, :lastname, :login, :password, :password_confirmation, :phone_number, :remember_me, :roles, :roles_mask, :signature, :street, :street_number, :zipcode)
+    params.require(:user).permit(:city, :comment, :confirmed_at, :country, :email, :firstname, :lastname, :login, :password, :password_confirmation, :phone_number, :remember_me, :roles_mask, :signature, :street, :street_number, :zipcode, :roles => [])
   end
-  
+
 end
